3373414432d23a288cd7a2025594340a5b237fc0
MdeMedina <mamedina770@gmail.com>
2025-11-29 19:01:13 -0300

    Primer Commit y Configuraciones de mongo y swagger
    Signed-off-by: MdeMedina <mamedina770@gmail.com>

---
+++ src/main/kotlin/com/backend/huertohogar/HuertohogarApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.huertohogar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HuertohogarApplication
+
+fun main(args: Array<String>) {
+	runApplication<HuertohogarApplication>(*args)
+}
